bd1186de581e380a522f42d4eda77de005083d24
CChevalier <cchevalier70@gmail.com>
2015-07-23 15:32:58 +0200

    Playing around with layouts

--- ui.R
+++ ui.R
@@ -1,28 +1,75 @@
 # ui.R
 
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
+  theme = shinytheme("united"),
   
-  # Application title
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      h2('sidebarPanel as h2'),
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+  navbarPage(
+    "My Application",
+    
+    tabPanel("Exp dist & CLT",
+      
+      # Application title
+      titlePanel("Exponential Distribution and Central Limit Theorem"),
+      
+      # sidebarLayout
+      sidebarLayout(
+        # Settings
+        sidebarPanel(
+          h4('Settings'),
+          
+          # Lambda
+          sliderInput(
+            "lambda",
+            "Lambda value:",
+            min = 0.1,
+            max = 1,
+            step = 0.1,
+            value = 0.2
+          ),
+          
+          sliderInput(
+            "bins",
+            "Number of exponentials per simulation:",
+            min = 10,
+            max = 50,
+            step = 5,
+            value = 25
+          ),
+          sliderInput(
+            "nosim",
+            "Number of simulations:",
+            min = 100,
+            max = 1000,
+            step = 50,
+            value = 500
+          )
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+          tabsetPanel(
+            tabPanel("Sample"),
+            tabPanel("Distribution", plotOutput("distPlot")),
+            tabPanel("Misc.")
+          )
+        )
+      )
     ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      h1('mainPanel title as h1'),
-      plotOutput("distPlot")
-    )
-  )
+    tabPanel("mtcars & lm",
+             
+             # Application title
+             titlePanel("Blabla")),
+    tabPanel("About & Help")
+  ),
+
+  hr(),
+  
+  fluidRow(column(6, offset = 1,
+                  h5("CChevalier")))
+  
 ))
